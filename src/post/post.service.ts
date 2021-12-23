68f3104a70953f3bdf0843bca34a75413a036c65
jianyanbo <97041634@qq.com>
2021-12-23 13:43:53 +0800

    服务：service

---
+++ src/post/post.service.ts
@@ -0,0 +1,18 @@
+/**
+ * 获取内容列表
+ */
+export const getPosts = () => {
+  const data = [
+    {
+      content: '内容1'
+    },
+    {
+      content: '内容2'
+    },
+    {
+      content: '内容3'
+    },
+  ];
+
+  return data;
+};
