12f16b9c535343b27dd6555e95ddbfe6614a0adb
jianyanbo <97041634@qq.com>
2021-12-24 12:08:06 +0800

    定义移除内容标签接口

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -25,6 +25,26 @@ router.post('/posts', authGuard, postController.store);
  */
   router.delete('/posts/:postId', authGuard, accessControl({ possession: true }), postController.destroy);
 
+  /**
+ * 添加内容标签
+ */
+router.post(
+  '/posts/:postId/tag', 
+  authGuard, 
+  accessControl({ possession: true }), 
+  postController.storePostTag
+);
+
+/**
+ * 移除内容标签
+ */
+ router.delete(
+  '/posts/:postId/tag', 
+  authGuard, 
+  accessControl({ possession: true }), 
+  postController.destroyPostTag
+);
+
 /**
  * 导出路由
  */
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -79,7 +79,7 @@ export const createPost = async (post: PostModel) => {
 /**
  * 保持内容标签
  */
- export const creatPostTag = async (
+ export const createPostTag = async (
   postId: number, tagId: number
 ) => {
   // 准备查询
@@ -113,4 +113,24 @@ export const createPost = async (post: PostModel) => {
 
   // 提供数据
   return data[0] ? true : false;
+};
+
+/**
+ * 移除内容标签
+ */
+ export const deletePostTag = async (
+  postId: number,
+  tagId: number
+) => {
+  // 准备查询
+  const statement = `
+    DELETE FROM post_tag
+    WHERE postId = ? AND tagId = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
+
+  // 提供数据
+  return data;
 };
