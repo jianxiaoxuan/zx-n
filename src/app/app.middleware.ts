5070a499906631121c6af22c69a520e2a791d0a1
jianyanbo <97041634@qq.com>
2021-12-25 06:21:59 +0800

    定义按ID调取内容的功能

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -63,6 +63,10 @@ export const defaultErrorHandler = (
       statusCode = 400;
       message = '内容已经有这个标签了';
       break;
+    case 'NOT_FOUND':
+      statusCode = 404;
+      message = '没找到 ～～ 🦖';
+      break;
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ～～ 🌴';
